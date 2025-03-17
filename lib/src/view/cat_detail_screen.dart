00a91852dc9d2fefad7696e78aab63b292102c04
daria <dadrobysheva@edu.hse.ru>
2025-03-17 20:11:24 +0300

    Updated design on detailed information screen

--- lib/src/view/cat_detail_screen.dart
+++ lib/src/view/cat_detail_screen.dart
@@ -9,47 +9,103 @@ class CatDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(cat.breed)),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+      ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Image.network(
-                  cat.imageUrl,
-                  height: 300,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
+              Transform.translate(
+                offset: Offset(-30, 0),
+                child: Row(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(25),
+                      child: Image.network(
+                        cat.imageUrl,
+                        height: 220,
+                        width: 220,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        _buildInfoBlock('Life Span', '${cat.lifeSpan} yr'),
+                        SizedBox(height: 8),
+                        _buildInfoBlock('Weight', '${cat.weight} kg'),
+                      ],
+                    ),
+                  ]
                 ),
               ),
-              SizedBox(height: 20),
-              Text(
-                'Breed: ' + cat.breed.toLowerCase(),
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 8),
-              Text(
-                'Weight: ' + cat.weight + ' kg',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 8),
-              Text(
-                'Life span: ' + cat.lifeSpan + ' years',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 8),
-              Text(
-                'Temperament: ' + cat.temperament.toLowerCase(),
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
+              SizedBox(height: 24),
+              _buildDetailBlock('Breed', cat.breed),
+              SizedBox(height: 16),
+              _buildDetailBlock('Temperament', cat.temperament),
             ],
           ),
-        ),
+        )
+
       ),
     );
   }
-}
 
+  Widget _buildInfoBlock(String title, String value) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          value,
+          style: TextStyle(
+            fontSize: 18,
+            fontFamily: 'Source Sans Pro',
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: 14,
+            fontFamily: 'Source Sans Pro',
+            fontWeight: FontWeight.normal,
+            color: Colors.grey,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDetailBlock(String title, String value) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: 20,
+            fontFamily: 'Source Sans Pro',
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        SizedBox(height: 8),
+        Text(
+          value,
+          style: TextStyle(
+            fontSize: 16,
+            fontFamily: 'Source Sans Pro',
+            fontWeight: FontWeight.normal,
+            color: Colors.black,
+          ),
+        ),
+      ],
+    );
+  }
+}
