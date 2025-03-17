6a7485a08a2490222db094cf01cdafb73f42ed8d
daria <dadrobysheva@edu.hse.ru>
2025-03-17 17:04:56 +0300

    Added cat model

---
+++ lib/src/model/cat.dart
@@ -0,0 +1,15 @@
+class Cat {
+  final String imageUrl;
+  final String breed;
+  final String weight;
+  final String lifeSpan;
+  final String temperament;
+
+  Cat({
+    required this.imageUrl,
+    required this.breed,
+    required this.weight,
+    required this.lifeSpan,
+    required this.temperament
+  });
+}
